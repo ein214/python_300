b240835d638296a85e6d96e8e472d2ee0befa14b
Eunji Ko <einee214@gmail.com>
2022-04-20 13:21:36 +0900

    first commit

---
+++ q1_10.py
@@ -0,0 +1,26 @@
+# q1
+print("Hello World")
+
+# q2
+print("Mary's cosmetics")
+
+# q3
+print('신씨가 소리질렀다. "도둑이야".')
+
+# q4
+print("C:\Windows")
+
+# q5
+# \t는 탭 \n은 줄바꿈
+
+# q7
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+# q8
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+# q9
+print("first", "second", end="")
+
+# q10
+print(5/3)
