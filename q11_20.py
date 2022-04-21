d29ba82f61f8737e208ce0f529f543d52e92949d
Eunji Ko <einee214@gmail.com>
2022-04-21 10:38:22 +0900

    파이썬 변수

---
+++ q11_20.py
@@ -0,0 +1,45 @@
+# q11
+from functools import total_ordering
+
+
+samsung = 50000
+print(samsung * 10)
+
+# q12
+total = 298000000000
+current = 50000
+percent = 15.79
+
+# q13
+s = "hello"
+t = "python"
+print(s+"!", t)
+
+# q15
+a = "132"
+print(type(a))
+
+# q16
+num_str = "720"
+print(int(num_str))
+
+# q17
+num = 100
+num_str = str(num)
+print(num_str, type(num_str))
+
+# q18
+str = "15.79"
+str_float = float(str)
+print(str_float, type(str_float))
+
+# q19
+year = "2020"
+year = int(year)
+print(year, year-1, year-2, year-3)
+
+# q20
+price = 48584
+month = 36
+total = price * month
+print(total)
